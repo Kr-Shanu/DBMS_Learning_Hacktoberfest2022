df980c58149d582da774de37ada180376e3956d0
SurajS27 <91777325+SurajS27@users.noreply.github.com>
2022-10-28 10:07:34 +0530

    Create 176_SecondHighestSalary.sql (#31)
    solution for leet code problem number 176

---
+++ Leetcode_DBMS_Questions/176_SecondHighestSalary.sql
@@ -0,0 +1,61 @@
+-- Surajkumar Sikchi
+-- Date: 28/10/2002
+
+--QUESTION:
+
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+--SOLUTION(CODE)
+
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (select max(salary) from Employee)
