d5e0b87a5dde05166eed024ddf55ad99c7800cfa
SurajS27 <91777325+SurajS27@users.noreply.github.com>
2022-10-28 12:18:11 +0530

    181_EmployeesEarningMoreThanTheirManagers.sql (#33)
    CODE for problem no 181 on leetcode

---
+++ Leetcode_DBMS_Questions/181_EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,52 @@
+-- Surajkumar Sikchi
+-- Date: 28/10/2022
+
+-- PROBLEM STATEMENT 
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write an SQL query to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+Explanation: Joe is the only employee who earns more than his manager.
+
+-- SOLUTION (CODE)
+
+SELECT e1.Name as Employee
+FROM Employee as e1, Employee as e2
+WHERE e1.ManagerId = e2.Id 
+  AND e1.Salary > e2.Salary;
