a7f08e085fbaa6df9b66b53118eca9060e5d7ac6
Suraj2707 <88773709+Suraj2707@users.noreply.github.com>
2022-10-30 00:05:46 +0530

    197_RisingTemperature.sql
    CODE FOR LEETCODE PROBLEM NUMBER 197

---
+++ Leetcode_DBMS_Questions/197_RisingTemperature.sql
@@ -0,0 +1,60 @@
+-- Surajkumar Sikchi
+-- Date: 30/10/2022
+
+-- PROBLEM STATEMENT
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+-- SOLUTION
+
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
